4f123a93eab9da1ccf438bfd9252eb7abb793cc7
Valeriya <sternviktoria36@gmail.com>
2025-02-11 10:07:31 +0300

    add:add chat controller

---
+++ Controller/ChatDB.php
@@ -0,0 +1,40 @@
+<?php
+namespace Controller;
+require_once __DIR__ . '/../autoload.php';
+use Model\Database;
+
+class ChatDB{
+        private $conn;
+
+    public function __construct(){
+        $this->conn = Database::getInstance()->getConnection();
+    }
+
+    public function getMessages($sortField, $sortOrder, $limit, $offset) {
+        $sql = "SELECT * FROM messages ORDER BY $sortField $sortOrder LIMIT :limit OFFSET :offset";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':limit', $limit, \PDO::PARAM_INT);
+        $stmt->bindParam(':offset', $offset, \PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+    
+
+    // public function sort($sortField, $sortOrder, $limit = 25, $offset){
+    //     $sql = "SELECT * FROM messages ORDER BY $sortField $sortOrder LIMIT :limit OFFSET :offset";
+    //     $stmt = $connect->prepare($sql);
+    //     $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+    //     $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+    //     $stmt->execute();
+    //     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // }
+
+    public function bbcodeToHtml($text) {
+        $text = preg_replace('/\*\*(.*?)\*\*/is', '<b>$1</b>', $text);
+        $text = preg_replace('/\*(.*?)\*/is', '<i>$1</i>', $text);
+        return $text;
+    }
+}
+
+
+
